f2c0677c9c052e86bd4fdb01af8eae0f73e8e8c6
Tom Arnfeld <tarnfeld@me.com>
2012-07-01 00:15:11 +0100

    test circuit

--- gpio/memory.py
+++ gpio/memory.py
@@ -1,5 +1,25 @@
+#!/usr/bin/python
+
 import time
 from RPi import GPIO
 
-# Logic here...
+# Define the connection pins
+OUTPUT_LED = 12
+INPUT_BUTTON = 8
+
+# Setup the connections
+GPIO.setup(OUTPUT_LED, GPIO.OUT)
+GPIO.setup(INPUT_BUTTON, GPIO.IN)
+
+GPIO.output(OUTPUT_LED, False)
+
+# Main Loop
+while 1:
+  time.sleep(1)
+  GPIO.output(OUTPUT_LED, True)
+  print "On"
+  time.sleep(1)
+  GPIO.output(OUTPUT_LED, False)
+  print "Off"
+  print "Button Status:", GPIO.input(INPUT_BUTTON)
 
